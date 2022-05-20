655e4480733e905d1acdc00717efc78febcaf69e
Debian <admin@ip-172-31-5-116.ap-south-1.compute.internal>
2022-05-20 08:59:48 +0000

    Add user input

--- first.py
+++ first.py
@@ -1,6 +1,9 @@
 l = ["ravi","jj","ek","rahul"]
 
-for i in l
-    print("Name: " i)
+for i in l:
+    print("Name: ",i)
 
 print("Writing forloop for list.....")
+
+name = input("Enter name:")
+print("Name:", name)

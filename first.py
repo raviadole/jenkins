b3529e24ec625a24f7d531276e34ac5a7300b500
raviadole <105899061+raviadole@users.noreply.github.com>
2022-05-20 14:48:08 +0530

    Update first.py

--- first.py
+++ first.py
@@ -3,7 +3,7 @@ l = ["ravi","jj","ek","rahul"]
 for i in l:
     print("Name: ",i)
 
-print("Writing forloop for list.....")
+print(Writing forloop for list.....")
 
 #name = input("Enter name:")
 #print("Name:", name)
